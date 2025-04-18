a581c02c7a4b99ecc4672903708b54f1a4ed1a47
Rasmeh27 <luisgnacio123@gmail.com>
2025-04-18 16:09:24 -0400

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Article from "./pages/Article";
-import Nav from "components/Nav";
 
 function App() {
   return (
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import Home from "./pages/Home";
 import Article from "./pages/Article";
 import Nav from "./components/Nav";
